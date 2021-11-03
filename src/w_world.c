d4b99beb74b95216ad03f7b1a655f9610c2189b0
EricKnudsen256 <ek256@njit.edu>
2021-11-03 11:20:57 -0400

    Starting room code

---
+++ src/w_world.c
@@ -0,0 +1,34 @@
+#include "w_world.h"
+
+World game_world = {0};
+
+void world_init(Uint32 max_rooms)
+{
+    
+}
+
+void world_draw(Uint32 bufferFrame, VkCommandBuffer commandBuffer)
+{
+    
+}
+
+void world_free()
+{
+    
+}
+
+Room* world_new_room(char *filename)
+{
+    
+}
+
+void world_free_room()
+{
+    
+}
+
+void world_layout_rooms();
+{
+    
+}
+
